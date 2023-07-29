78c170d06571ea1d4adde1a548644aa8c9b4245a
Oleg <olgklm07@gmail.com>
2023-07-29 14:06:04 +0300

    Thank to ChatGPT

--- xailoxshop/app_xailoxshop/models.py
+++ xailoxshop/app_xailoxshop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class xailox(models.Model):
+    title = models.CharField('Заголовок',max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField('Цена',max_digits=12, decimal_places=2)
+    auction = models.BooleanField('Торг',help_text='Поставьте галочку, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
+    
+    
+
+    class Meta:
+        db_table = 'xailox'
+    
