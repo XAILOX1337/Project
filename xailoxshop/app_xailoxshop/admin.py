bfd70c863660599f861ce7a92131000aca076ee2
Oleg <olgklm07@gmail.com>
2023-08-08 13:09:29 +0300

    panelAdmin

--- xailoxshop/app_xailoxshop/admin.py
+++ xailoxshop/app_xailoxshop/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import xailox
+from django.utils import timezone
 # Register your models here.
+class Admin(admin.ModelAdmin):
+    list_display = ['id','title', 'description','price','created_date','updated_date','auction']
+    list_filter = ['price','created_at','auction']
+    actions = ['make_auction_as_false','make_auction_as_true']
+     
+    
+
+    @admin.action(description='- Торг(Не для кавказцев)')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+    
+    @admin.action(description='+ Торг(Для кавказцев)')
+    def make_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
+    
+    fieldsets = (('Общее',{'fields':('title','description')}),('Финансы',{'fields':('price','auction'),'classes':['collapse']}))
+
+admin.site.register(xailox, Admin)
--- xailoxshop/app_xailoxshop/models.py
+++ xailoxshop/app_xailoxshop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib import admin
+from django.utils.html import format_html 
 # Create your models here.
 class xailox(models.Model):
     title = models.CharField('Заголовок',max_length=128)
@@ -8,9 +9,27 @@ class xailox(models.Model):
     auction = models.BooleanField('Торг',help_text='Поставьте галочку, если торг уместен')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    def __str__(self):
-        return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
     
+    
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time)
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+    
+
+    @admin.display(description='Дата редактирования')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html('<span style="color: #98F5FF; font-weight: bold;">Сегодня в {}</span>', updated_time)
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    def __str__(self):
+        return f'<Xailox: xailox(id={self.id}, title={self.title}, price={self.price})>'
     
 
     class Meta:
